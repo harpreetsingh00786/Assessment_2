b39f54ed2324ed5b32c6186c91b33269df26487c
@harpreetsingh00786 <harpreetsingh29101995@gmail.com>
2023-09-27 13:49:29 +0500

    Initial Commit

---
+++ StudentStatistics.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Assignment2 here.
+ *      This is the student statistics system, the user can load a txt file, calculate the total marks of each student, and 
+ *  display below the user threshold, the 5 highest and 5 lowest marks student details.
+ *
+ * @author (Harpreet Singh)
+ * @version (25/09/2023)
+ */
+
+class Student {
+    private String name;
+    private String studentID;
+    private int[] assessmentMarks;
+
+    public Student(String name, String studentID, int[] assessmentMarks) {
+        this.name = name;
+        this.studentID = studentID;
+        this.assessmentMarks = assessmentMarks;
+    }
+}
+public class StudentStatistics
+{
+    
+}
